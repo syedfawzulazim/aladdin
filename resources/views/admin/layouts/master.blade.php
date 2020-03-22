46580edb31a29ffb46a1dd0822e837c1fb71c1ac
Adnan Sparrow <adnansyed27@gmail.com>
2020-03-22 22:00:03 +0600

    up

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Aladdin || Admin</title>
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  @include('admin.partials.link')
+</head>
+
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+
+  <!-- Main Header -->
+  @include('admin.partials.header')
+  <!-- Left side column. contains the logo and sidebar -->
+  @include('admin.partials.slider')
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+
+    @yield('content')
+    @yield('res')
+
+  </div>
+
+  <!-- Main Footer -->
+  @include('admin.partials.footer')
+
+  <!-- Control Sidebar -->
+  @include('admin.partials.slidebar')
+
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED JS SCRIPTS -->
+@include('admin.partials.script')
+
+</body>
+</html>
---
+++ resources/views/admin/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+  <!-- To the right -->
+  <div class="pull-right hidden-xs">
+  <a href="#"> AdnanSparrow</a>
+  </div>
+  <!-- Default to the left -->
+  <strong>Copyright &copy; 2020 <b>All rights reserved</b> </strong>
+</footer>
