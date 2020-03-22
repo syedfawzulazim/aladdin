ad70ca14943e557773bdd6bfd75c13f080056963
Adnan Sparrow <adnansyed27@gmail.com>
2020-03-22 22:04:48 +0600

    Revert "up"
    This reverts commit 46580edb31a29ffb46a1dd0822e837c1fb71c1ac.

--- resources/views/admin/partials/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-  <!-- To the right -->
-  <div class="pull-right hidden-xs">
-  <a href="#"> AdnanSparrow</a>
-  </div>
-  <!-- Default to the left -->
-  <strong>Copyright &copy; 2020 <b>All rights reserved</b> </strong>
-</footer>
